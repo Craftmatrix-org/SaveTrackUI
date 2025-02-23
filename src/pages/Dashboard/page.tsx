9152adb01006ba6599262e87d5afbe5a4e865df8
renzaspiras <aspirasrenz@gmail.com>
2025-02-24 01:58:15 +0800

    initialized token API

--- src/pages/Dashboard/page.tsx
+++ src/pages/Dashboard/page.tsx
@@ -1,5 +1,7 @@
 import { useAtom } from "jotai";
 import { AtomEmail } from "../../atom/GateAtom";
+import { useEffect } from "react";
+import axios from "axios";
 
 export const Dashboard = () => {
   const [email] = useAtom(AtomEmail);
@@ -8,6 +10,23 @@ export const Dashboard = () => {
   // check email if exist in database
   // if true generate token
   // else register, and then post the new email, then generate token
+
+  useEffect(() => {
+    const fetchToken = async () => {
+      try {
+        const response = await axios.post(
+          `http://localhost:5184/api/v1/UserToken/${email}`,
+        );
+        document.cookie = `token=${response.data}; path=/`;
+      } catch (error) {
+        console.error("Error fetching token:", error);
+      }
+    };
+
+    if (email) {
+      fetchToken();
+    }
+  }, [email]);
 
   return (
     <>
