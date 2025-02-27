bac11eb6ca572961db5486a1b0211795d2be4209
renzaspiras <aspirasrenz@gmail.com>
2025-02-27 15:20:55 +0800

    Refined Category

--- src/pages/Dashboard/categories/page.tsx
+++ src/pages/Dashboard/categories/page.tsx
@@ -14,6 +14,12 @@ import { getTokenDataFromCookie } from "../../../api/token";
 import { useEffect, useState } from "react";
 import { Edit } from "./crud/edit";
 import { Delete } from "./crud/delete";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "../../../components/Accordion";
 
 export const Categories = () => {
   const [category] = useAtom(AtomCategory);
@@ -30,59 +36,113 @@ export const Categories = () => {
     categoryItem.name.toLowerCase().includes(searchTerm.toLowerCase()),
   );
 
+  const depositCategories = filteredCategories.filter(
+    (categoryItem) => categoryItem.isPositive,
+  );
+
+  const withdrawCategories = filteredCategories.filter(
+    (categoryItem) => !categoryItem.isPositive,
+  );
+
   return (
-    <div>
-      <div className="flex flex-col gap-2 w-full p-1">
-        <div className="m-2 flex flex-row w-full sm:w-[60%] mx-auto gap-1">
-          <TextField.Root
-            placeholder="Search for Category"
-            className="grow"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-          <Add />
-        </div>
-        {filteredCategories.map((categoryItem) => (
-          <Box key={categoryItem.id} className="mx-auto w-full sm:w-[60%]">
-            <Card>
-              <Flex gap="3" align="center">
-                <Avatar
-                  size="3"
-                  src="https://assets.techrepublic.com/uploads/2021/08/tux-new.jpg"
-                  radius="full"
-                  fallback="T"
-                />
-                <Box>
-                  <Text as="div" size="2" weight="bold">
-                    {categoryItem.name.toString()} |{" "}
-                    <Badge color={categoryItem.isPositive ? "blue" : "red"}>
-                      {categoryItem.isPositive ? "deposit" : "withdraw"}
-                    </Badge>
-                  </Text>
-                  <Text as="div" size="2" color="gray">
-                    {categoryItem.description}
-                  </Text>
-                </Box>
-
-                <div className="flex flex-row items-center gap-1 ml-auto">
-                  <Text as="div" size="2" color="gray">
-                    {new Date(categoryItem.updatedAt).toLocaleDateString(
-                      "en-US",
-                      {
-                        month: "short",
-                        day: "2-digit",
-                        year: "numeric",
-                      },
-                    )}{" "}
-                  </Text>
-
-                  <Edit categoryId={categoryItem.id} />
-                  <Delete categoryId={categoryItem.id} />
-                </div>
-              </Flex>
-            </Card>
-          </Box>
-        ))}
+    <div className="flex flex-col gap-2 w-full p-1">
+      <div className="m-2 flex flex-row w-full sm:w-[60%] mx-auto gap-1">
+        <TextField.Root
+          placeholder="Search for Category"
+          className="flex-grow"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <Add />
+      </div>
+      <div className="flex flex-col sm:flex-row w-full sm:w-[60%] mx-auto gap-1">
+        <Accordion
+          type="single"
+          collapsible
+          className="w-full"
+          defaultValue="deposit"
+        >
+          <AccordionItem value="deposit">
+            <AccordionTrigger>Deposit</AccordionTrigger>
+            <AccordionContent>
+              {depositCategories.map((categoryItem) => (
+                <Card key={categoryItem.id} className="mb-2">
+                  <Flex gap="3" align="center">
+                    <Avatar
+                      size="3"
+                      src="https://assets.techrepublic.com/uploads/2021/08/tux-new.jpg"
+                      radius="full"
+                      fallback="T"
+                    />
+                    <Box>
+                      <Text as="div" size="2" weight="bold">
+                        {categoryItem.name.toString()} |{" "}
+                        <Badge color="blue">deposit</Badge>
+                      </Text>
+                      <Text as="div" size="2" color="gray">
+                        {categoryItem.description}
+                      </Text>
+                    </Box>
+                    <div className="flex flex-row items-center gap-2 ml-auto">
+                      <Text as="div" size="2" color="gray">
+                        {new Date(categoryItem.updatedAt).toLocaleDateString(
+                          "en-US",
+                          {
+                            month: "short",
+                            day: "2-digit",
+                            year: "numeric",
+                          },
+                        )}{" "}
+                      </Text>
+                      <Edit categoryId={categoryItem.id} />
+                      <Delete categoryId={categoryItem.id} />
+                    </div>
+                  </Flex>
+                </Card>
+              ))}
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="withdraw">
+            <AccordionTrigger>Withdraw</AccordionTrigger>
+            <AccordionContent>
+              {withdrawCategories.map((categoryItem) => (
+                <Card key={categoryItem.id} className="mb-2">
+                  <Flex gap="3" align="center">
+                    <Avatar
+                      size="3"
+                      src="https://assets.techrepublic.com/uploads/2021/08/tux-new.jpg"
+                      radius="full"
+                      fallback="T"
+                    />
+                    <Box>
+                      <Text as="div" size="2" weight="bold">
+                        {categoryItem.name.toString()} |{" "}
+                        <Badge color="red">withdraw</Badge>
+                      </Text>
+                      <Text as="div" size="2" color="gray">
+                        {categoryItem.description}
+                      </Text>
+                    </Box>
+                    <div className="flex flex-row items-center gap-2 ml-auto">
+                      <Text as="div" size="2" color="gray">
+                        {new Date(categoryItem.updatedAt).toLocaleDateString(
+                          "en-US",
+                          {
+                            month: "short",
+                            day: "2-digit",
+                            year: "numeric",
+                          },
+                        )}{" "}
+                      </Text>
+                      <Edit categoryId={categoryItem.id} />
+                      <Delete categoryId={categoryItem.id} />
+                    </div>
+                  </Flex>
+                </Card>
+              ))}
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
       </div>
     </div>
   );
