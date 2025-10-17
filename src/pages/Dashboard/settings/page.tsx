6a8433a225d6e2af520627cb438d53e2060a3fd2
renzaspiras <recruas@aspirasrenz.me>
2025-10-18 04:59:37 +0800

    full logout implementation of authentik

--- src/pages/Dashboard/settings/page.tsx
+++ src/pages/Dashboard/settings/page.tsx
@@ -1,18 +1,25 @@
-import { Button } from "@radix-ui/themes"
+import { Button } from "@radix-ui/themes";
 
 export const Settings = () => {
   const logout = () => {
-    document.cookie.split(';').forEach(cookie => {
-      const eqPos = cookie.indexOf('=');
+    document.cookie.split(";").forEach((cookie) => {
+      const eqPos = cookie.indexOf("=");
       const name = eqPos > -1 ? cookie.substring(0, eqPos) : cookie;
-      document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT';
+      document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
     });
-  }
-  return (<>
-    <Button
-      onClick={() => {
-        logout()
-      }}
-    >Logout</Button>
-  </>)
-}
+
+    window.location.href =
+      "https://bridge.craftmatrix.org/logout?app=savetrack";
+  };
+  return (
+    <>
+      <Button
+        onClick={() => {
+          logout();
+        }}
+      >
+        Logout
+      </Button>
+    </>
+  );
+};
