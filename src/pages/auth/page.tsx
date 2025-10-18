9d86cfe2b4012bfc676dabf0431e8344014b0651
renzaspiras <recruas@aspirasrenz.me>
2025-10-19 02:07:39 +0800

    token digestion

--- src/pages/auth/page.tsx
+++ src/pages/auth/page.tsx
@@ -1,9 +1,16 @@
+import { saveTokenToCookie } from "@/lib/token";
 import { useParams } from "react-router-dom";
+import { useEffect } from "react";
 
 export const Auth = () => {
   const { value } = useParams();
 
-  console.log("Value:", value);
+  useEffect(() => {
+    if (value) {
+      console.log("Value:", value);
+      saveTokenToCookie(value);
+    }
+  }, [value]); // only run when 'value' changes
 
   return <div>Param: {value}</div>;
 };
